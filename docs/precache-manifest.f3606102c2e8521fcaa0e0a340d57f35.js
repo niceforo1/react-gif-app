ef4d53ebaabac15a515cc688b3ed1227c515aea6
Gabino Santana <gabino_18_@hotmail.com>
2020-07-30 22:49:24 -0300

    docs agregados

---
+++ docs/precache-manifest.f3606102c2e8521fcaa0e0a340d57f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff2df9597ce0aeac78a12b5a45a6f32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6e97cff418db080f68",
+    "url": "/static/css/main.219fdd44.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6e97cff418db080f68",
+    "url": "/static/js/main.5a59b765.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
